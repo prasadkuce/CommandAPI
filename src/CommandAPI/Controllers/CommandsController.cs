b601d46959813a8da84139dc7acd8b72cc5d4bbb
Prasad Gopishetty <prasadkuce@gmail.com>
2020-11-23 19:16:09 +0530

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "This", "is", "Hard", "Coded" };
+        }
+    }
+}
